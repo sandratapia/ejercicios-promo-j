3f376a4fe1d5acb36fb3ed4e7c3a729f4cc4c7fd
sandra1894 <65424709+sandra1894@users.noreply.github.com>
2020-06-19 10:11:51 +0200

    exercise 6

---
+++ modulo-2/modulo2-leccion2.1-ejercicio-6/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const title = document.querySelector(".introduce");
+const content = '<h1>Lorem ipsum</h1><img src="http://via.placeholder.com/350x150" alt="imagen"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>'
+
+title.innerHTML = content;
